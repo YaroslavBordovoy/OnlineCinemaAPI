28f3f83b03c390495d3c893af316e467b58d0d07
mregorlite <egor.infinity@gmail.com>
2025-01-31 19:30:02 +0200

    feat: create migration

--- src/database/models/orders.py
+++ src/database/models/orders.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import (
     relationship
 )
 from database.models.base import Base
-from database.models.users import UserModel
+from database.models.accounts import UserModel
 from database.models.movies import MovieModel
 
 

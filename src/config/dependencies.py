4c9f47df602d7273e05443e9b72552384f0b4d4c
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-01-31 17:52:20 +0200

    Fix imports

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -1,6 +1,6 @@
 import os
 
-from config.settings import TestingSettings, Settings, BaseAppSettings
+from src.config.settings import TestingSettings, Settings, BaseAppSettings
 
 
 def get_settings() -> BaseAppSettings:

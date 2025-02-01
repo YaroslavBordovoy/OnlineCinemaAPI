12f57e92d3c57e66b7f26e65271cafc168f223cd
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2025-02-01 12:28:49 +0200

    feat: Add run server in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from routes import movie_router
@@ -12,3 +13,7 @@ app = FastAPI(
 api_version_prefix = "/api/v1"
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/cinema", tags=["cinema"])
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)

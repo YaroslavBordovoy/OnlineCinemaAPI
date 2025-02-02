78f067cd313efc275e0024d8ef9ba5cde117ed80
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-02-03 00:57:00 +0200

    Add needed configurations

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -6,6 +6,7 @@ from fastapi import Depends
 from config.settings import TestingSettings, Settings, BaseAppSettings
 from security.jwt_interface import JWTAuthManagerInterface
 from security.token_manager import JWTAuthManager
+from storages import S3StorageInterface, S3StorageClient
 
 
 load_dotenv()
@@ -23,4 +24,13 @@ def get_jwt_auth_manager(settings: BaseAppSettings = Depends(get_settings)) -> J
         secret_key_access=settings.SECRET_KEY_ACCESS,
         secret_key_refresh=settings.SECRET_KEY_REFRESH,
         algorithm=settings.JWT_SIGNING_ALGORITHM
+    )
+
+
+def get_s3_storage_client(settings: BaseAppSettings = Depends(get_settings)) -> S3StorageInterface:
+    return S3StorageClient(
+        endpoint_url=settings.S3_STORAGE_ENDPOINT,
+        access_key=settings.S3_STORAGE_ACCESS_KEY,
+        secret_key=settings.S3_STORAGE_SECRET_KEY,
+        bucket_name=settings.S3_BUCKET_NAME
     )

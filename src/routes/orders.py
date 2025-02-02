4e34d3418c2fd2df57d93daf9e10757a271ffad1
mregorlite <egor.infinity@gmail.com>
2025-02-02 23:14:32 +0200

    feat: Add route orders/{order_id}/

--- src/routes/orders.py
+++ src/routes/orders.py
@@ -207,3 +207,53 @@ def get_all_orders(
     return OrderListResponseSchema(
         orders=orders,
     )
+
+
+@router.get(
+    "/{order_id}/",
+    response_model=OrderResponseSchema
+)
+def get_order(
+        order_id: int,
+        token: str = Depends(get_token),
+        jwt_manager=Depends(get_jwt_auth_manager),
+        db: Session = Depends(get_db)
+):
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        user_id = payload.get("user_id")
+    except BaseSecurityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
+        )
+
+    user = db.query(UserModel).join(UserGroupModel).filter(UserModel.id == user_id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token or user not found."
+        )
+
+    order = (
+        db
+        .query(OrderModel)
+        .join(OrderItemModel)
+        .filter(OrderModel.id == order_id)
+        .first()
+    )
+
+    if not order:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Order not found."
+        )
+
+    if order.user_id != user.id and user.group.name != UserGroupEnum.ADMIN:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have permission to access this resource."
+        )
+
+    return order

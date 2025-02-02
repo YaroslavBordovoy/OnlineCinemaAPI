3a51ff9d5683749492f481e7774d0486c9077725
Volodymyr <uuforreasonsunknownuu@gmail.com>
2025-02-02 22:29:41 +0200

    feat: created routes and schemas for add, delete, clear items from cart and detail endpoint for cart

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,3 +2,4 @@ from routes.movies import router as movie_router
 from routes.accounts import router as accounts_router
 from routes.orders import router as order_router
 from routes.payments import router as payments_router
+from routes.carts import router as carts_router
